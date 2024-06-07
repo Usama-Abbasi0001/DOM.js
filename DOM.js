06eb6e01f3b0cbac723353f1d44c926517aa3f1d
unknown <usamakhan58461@gmail.com>
2024-06-07 10:24:23 +0500

    DOM.js

---
+++ DOM.js
@@ -0,0 +1,13 @@
+var hade = document.getElementById("header");
+hade.innerHTML="Usama Khan";
+hade.style.fontSize="50px"
+hade.style.display="block"
+
+
+var yahu = document.getElementsByTagName("li");
+yahu[2].textContent= "About"
+yahu[0].textContent = "Home";
+yahu[1].textContent = "Contact";
+yahu[3].textContent = "Services";
+yahu[2].style.color='green'
+yahu[3].style.fontSize = "30px";
